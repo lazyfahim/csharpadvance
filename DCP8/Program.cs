4ee2fc9d0234bc05ee88baf0e503e08b4312fe9f
Syed Mohammad Fahim Abrar <fahimabrar13@gmail.com>
2020-02-10 13:01:39 +0600

    folder arranged

--- DCP8/Program.cs
+++ DCP8/Program.cs
@@ -1,56 +1,86 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Numerics;
 
-namespace DCP1
+namespace DCP8
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            String s;
-#if !ONLINE_JUDGE
-            Console.SetIn(File.OpenText("file.txt"));
-            Console.SetOut(File.CreateText("out.txt"));
-#endif
-            while ((s = Console.ReadLine()) != null)
+            //Console.SetIn(File.OpenText("file.txt"));
+            //Console.SetOut(File.CreateText("out.txt"));
+            int p = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < p; i++)
             {
-                string[] numbers = s.Split(',');
-                string firstNumber =  numbers[0];
-                string secondNumber = numbers[1];
-                string sumstring = "";
-                int len = firstNumber.Length > secondNumber.Length ? firstNumber.Length : secondNumber.Length;
-                int extra = 0;
-                for (int i = 0; i < len; i++)
-                { 
-                    int first = i < firstNumber.Length ? Int32.Parse(firstNumber.ElementAt(i).ToString()) : 0;
-                    int second = i < secondNumber.Length ? Int32.Parse(secondNumber.ElementAt(i).ToString()) : 0;
-                    int sum = first + second+extra;
-                    if (i == len - 1)
-                    {
-                        sumstring += ReverseString(sum.ToString());
-                        continue;
-                    }
-                    sumstring += (sum % 10).ToString();
-                    extra = sum / 10;
-                }
-                
-                sumstring = sumstring.TrimStart(new char[] { '0' });
-
-
-                Console.WriteLine(sumstring);
+                Console.WriteLine("Case: {0}",i+1);
+                Solve();
             }
         }
-        public static string ReverseString(string s)
+
+        private static void Solve()
         {
-            char[] array = new char[s.Length];
-            int forward = 0;
-            for (int i = s.Length - 1; i >= 0; i--)
+            int n;
+            n = Convert.ToInt32(Console.ReadLine());
+            var originalNumers = new List<string>();
+            for (var i = 0; i < n; i++)
             {
-                array[forward++] = s[i];
+                var number = Console.ReadLine();
+                originalNumers.Add(number);
             }
-            return new string(array);
+
+            var deliveryReports = new List<string>();
+            for (var i = 0; i < n; i++)
+            {
+                var number = Console.ReadLine();
+                deliveryReports.Add(number);
+            }
+
+            var SuccessList = new List<string>();
+            var FailedList = new List<string>();
+            foreach (var report in deliveryReports)
+            {
+                var numberString = report.Split(',');
+                var cache = "";
+                var flag = -1;
+                foreach (var original in originalNumers)
+                {
+                    if(original.Length != numberString[0].Length)
+                        continue;
+                    var first = original.Substring(0, 3);
+                    var second = original.Substring(7, original.Length-7);
+                    var firstofStarred = numberString[0].Substring(0, 3);
+                    var secondofStarred = numberString[0].Substring(7, numberString[0].Length-7);
+                    if (first == firstofStarred && second == secondofStarred)
+                    {
+                        if (flag == 1)
+                        {
+                            flag = 3;
+                            break;
+                        }
+
+                        cache = original;
+                        flag = 1;
+                    }
+                }
+
+                var finalString = "";
+                finalString += numberString[0]+" : ";
+                if (flag == 3)
+                    finalString += "Confused";
+                else
+                    finalString += cache;
+                if (numberString[1] == "Success")
+                    SuccessList.Add(finalString);
+                else
+                    FailedList.Add(finalString);
+            }
+
+            Console.WriteLine("Success list");
+            foreach (var sucess in SuccessList) Console.WriteLine(sucess);
+            Console.WriteLine("Failed list");
+            foreach (var falied in FailedList) Console.WriteLine(falied);
+            Console.ReadLine();
         }
     }
 }
