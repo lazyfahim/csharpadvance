2b4577403d4a07813a4429ded03ef881a0c7ded5
lazyfahim <fahimabrar13@gmail.com>
2020-06-04 20:17:48 +0600

    email and delegate added

---
+++ DelExample/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DelExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator(200,15);
+            var calcdel = new Calculator.Calculate(add);
+            var calcdelsub = new Calculator.Calculate(sub);
+            calc.Result(calcdel);
+            calc.Result(calcdelsub);
+        }
+        static void add(int a,int b)
+        {
+            Console.WriteLine(a+b);
+        }
+        static void sub(int a, int b)
+        {
+            Console.WriteLine(a-b);
+        }
+    }
+
+    public class Calculator
+    {
+        private int a,b;
+        public Calculator(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public delegate void Calculate(int a, int b);
+        public void Result(Calculate calculate)
+        {
+            calculate(a,b);
+        }
+
+    }
+}
---
+++ EventExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace EventExample
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            notifier.NotifyEvent += new Notifier.NotificationDelegate(new EmailNotification().Notify);
+            notifier.NotifyEvent += new Notifier.NotificationDelegate(new MobileNotification().Notify);
+            notifier.SendNotification("email","Hello Email");
+        }
+    }
+
+    class Notifier
+    {
+        public delegate void NotificationDelegate(string career, string message);
+        public event NotificationDelegate NotifyEvent;
+        public void SendNotification(string career,string message)
+        {
+            NotifyEvent(career,message);
+        }
+    }
+    class EmailNotification
+    {
+        public void Notify(string career,string message)
+        {
+            Console.WriteLine($"Sending to Email Address => {career}");
+            Console.WriteLine("Message: "+message);
+        }
+    }
+    class MobileNotification
+    {
+        public void Notify(string career, string message)
+        {
+            Console.WriteLine($"Sending to Mobile Career Mobile Number => {career}");
+            Console.WriteLine("Message: "+message);
+        }
+    }
+}
