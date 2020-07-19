0fa9aef114fa79f6cd1eebee894c4fd23f6ff52b
lazyfahim <fahimabrar13@gmail.com>
2020-07-19 17:01:27 +0600

    fixed untracked files

---
+++ Lambada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System;
+
+namespace Lambada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //lambada expression with delegate 
+            Calculator calc = new Calculator();
+            calc.DoSum(((x,y) => 
+            {
+                return x*y;
+            }),5,6);
+        }
+    }
+    class Calculator
+    {
+        public delegate int Sum(int a, int b);
+        public void DoSum(Sum sum,int a,int b)
+        {
+            Console.WriteLine(sum.Invoke(a,b));
+        }
+    }
+}
