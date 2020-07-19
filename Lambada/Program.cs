62380fa195c3c167497b4c1f6349b0cbb2d629bf
lazyfahim <fahimabrar13@gmail.com>
2020-07-19 17:20:44 +0600

    expression with func and inline

--- Lambada/Program.cs
+++ Lambada/Program.cs
@@ -7,18 +7,27 @@ namespace Lambada
     {
         static void Main(string[] args)
         {
-            //lambada expression with delegate 
+            //lambada expression inline with delegate 
             Calculator calc = new Calculator();
+            calc.DoSum(((x,y) => x+y),5,6);
+            //lambada expression multiple statement with delegate 
             calc.DoSum(((x,y) => 
             {
-                return x*y;
-            }),5,6);
+                return x+y;
+            }),7,8);
+
+            //lambada expression with Func 
+            calc.DoSumF((x,y)=> x+y,10,5);
         }
     }
     class Calculator
     {
         public delegate int Sum(int a, int b);
         public void DoSum(Sum sum,int a,int b)
+        {
+            Console.WriteLine(sum.Invoke(a,b));
+        }
+        public void DoSumF(Func<int,int,int> sum,int a,int b)
         {
             Console.WriteLine(sum.Invoke(a,b));
         }
